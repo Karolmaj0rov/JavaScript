b0aa97d07a217a370ae9311f16c2789c70f5dbad
Karolmaj0rov <nurmuhammadjonazimov@gmail.com>
2024-07-30 08:00:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+let name = prompt('isimni kirit')
+
+let surname = prompt('familiyani kirit')
+
+let age = prompt('yoshni kirit')
+
+let number = prompt('raqamni kirit')
+
+let maktab = prompt('maktabni kirit')
+
+let sana = prompt('yil sana')
+
+let manzil = prompt('manzilni kirit')
+
+let MarsdagiUstoz = prompt('Ustozni ismi')
+
+console.log(name)
+console.log(surname)
+console.log(age)
+console.log(number)
+console.log(maktab)
+console.log(sana)
+console.log(manzil)
+console.log(MarsdagiUstoz)
